774b2a64258b52439a1f6b57a51b980516e29a32
euwest <harwood.connor@gmail.com>
2016-04-11 14:07:57 -0700

    build basic Stipulate class functionality and tests
    prefix/postfix extension testing will come next, as a separate test file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-export default () => true
+import defaultsDeep from 'lodash.defaultsdeep';
+
+class Stipulate {
+
+  constructor(options) {
+    this.baseOptions = options;
+  }
+
+  prefix(request) {
+    return request;
+  }
+
+  postfix(response) {
+    return response;
+  }
+
+  send(url, options) {
+    const config = defaultsDeep({}, options, this.baseOptions);
+    const request = new Request(url, config);
+
+    return fetch(request);
+  }
+}
+
+export default Stipulate
--- test/index.js
+++ test/index.js
@@ -1,6 +1,96 @@
-import {expect} from 'chai';
-import works from '../src';
+import chai, {expect} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import Stipulate from '../src';
 
-it('runs tests', () => {
-  expect(works()).to.be.true;
+chai.use(chaiAsPromised);
+
+describe('Stipulate class', () => {
+  it('holds onto base options you instantiate it with', () => {
+    const options = {
+      foo: 'bar'
+    };
+    const stipulateFoo = new Stipulate(options);
+
+    expect(stipulateFoo.baseOptions).to.deep.equal(options);
+  });
+
+  describe('prefix method', () => {
+    it('passes a request through untouched, by default', () => {
+      const request = {
+        fizz: 'buzz'
+      };
+      const stipulate = new Stipulate();
+
+      expect(stipulate.prefix(request)).to.deep.equal(request);
+    });
+  });
+
+  describe('postfix method', () => {
+    it('passes a response through untouched, by default', () => {
+      const response = {
+        hello: 'world'
+      };
+      const stipulate = new Stipulate();
+
+      expect(stipulate.postfix(response)).to.deep.equal(response);
+    });
+  });
+
+  describe('send method', () => {
+    before(() => {
+      global.fetch = (request) => {
+        return Promise.resolve(request);
+      };
+
+      global.Request = function(url, options) {
+        this.url = url;
+        this.options = options;
+      };
+    });
+
+    after(() => {
+      delete global.fetch;
+      delete global.Request;
+    });
+
+    it('calls Fetch with a Request for the URL and any options given', () => {
+      const stipulate = new Stipulate();
+      const url = '/foo';
+      const opts = {
+        bar: 'baz'
+      };
+      const expected = new Request(url, opts);
+      const results = stipulate.send(url, opts);
+
+      return expect(results).to.eventually.deep.equal(expected);
+    });
+
+    it('merges options given with base options', () => {
+      const stipulate = new Stipulate({ foo: 'bar' });
+      const url = '/foo';
+      const opts = {
+        fizz: 'buzz'
+      };
+      const combinedOpts = {
+        foo: 'bar',
+        fizz: 'buzz'
+      };
+      const expected = new Request(url, combinedOpts);
+      const results = stipulate.send(url, opts);
+
+      return expect(results).to.eventually.deep.equal(expected);
+    });
+
+    it('prioritizes latest given options when duplicating base options', () => {
+      const stipulate = new Stipulate({ foo: 'bar' });
+      const url = '/foo';
+      const opts = {
+        foo: 'baz'
+      };
+      const expected = new Request(url, opts);
+      const results = stipulate.send(url, opts);
+
+      return expect(results).to.eventually.deep.equal(expected);
+    });
+  });
 });
