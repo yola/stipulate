486e2cee768bd4d08e6aa0b33d07971a650aa99f
euwest <harwood.connor@gmail.com>
2016-04-11 14:06:24 -0700

    style corrections: semi-colons, multi-line objects

--- src/make-ok-check.js
+++ src/make-ok-check.js
@@ -1,14 +1,14 @@
-function makeOkCheck({ okCodes, test } = {}) {
+function makeOkCheck({ okCodes = [], test } = {}) {
   return (response) => {
-    if(test && test(response)) return response
+    if(test && test(response)) return response;
 
-    let codeOk = okCodes && okCodes.includes(response.status)
+    const codeOk = okCodes.includes(response.status);
 
-    if(!test && (response.ok || codeOk)) return response
+    if(!test && (response.ok || codeOk)) return response;
 
-    let error = new Error()
+    const error = new Error();
 
-    throw error
+    throw error;
   }
 }
 
--- test/make-ok-check.js
+++ test/make-ok-check.js
@@ -1,76 +1,101 @@
-import makeOkCheck from '../src/make-ok-check'
-import {expect} from 'chai'
+import makeOkCheck from '../src/make-ok-check';
+import {expect} from 'chai';
 
 
 describe('makeOkCheck', () => {
   describe('given no options', () => {
-    let defaultCheck
+    let defaultCheck;
 
-    before(() => defaultCheck = makeOkCheck())
+    before(() => defaultCheck = makeOkCheck());
 
     it('defaults to checking http status is ok when given no opts', () => {
-      let okResponse = { ok: true }
+      const okResponse = { ok: true };
 
-      expect(defaultCheck(okResponse)).to.deep.equal(okResponse)
-    })
+      expect(defaultCheck(okResponse)).to.deep.equal(okResponse);
+    });
 
     it('defaults to throwing an error when not ok', () => {
-      let badResponse = { ok: false }
-      let checkNotOk = () => defaultCheck(badResponse)
+      const badResponse = {
+        ok: false
+      };
+      const checkNotOk = () => defaultCheck(badResponse);
 
-      expect(checkNotOk).to.throw(Error)
-    })
-  })
+      expect(checkNotOk).to.throw(Error);
+    });
+  });
 
   describe('given status codes to accept', () => {
-    let statusCodeCheck
-
-    before(() => statusCodeCheck = makeOkCheck({ okCodes: [403, 500] }))
-
-    it('still passes response if status ok', () => {
-      let okResponse = { ok: true }
-
-      expect(statusCodeCheck(okResponse)).to.deep.equal(okResponse)
-    })
-
-    it('passes response if status not ok but code is acceptable', () => {
-      let fourOhThree = { ok: false, status: 403 }
-      let fiveHundred = { ok: false, status: 500 }
-
-      expect(statusCodeCheck(fourOhThree)).to.deep.equal(fourOhThree)
-      expect(statusCodeCheck(fiveHundred)).to.deep.equal(fiveHundred)
-    })
-
-    it('throws if status not ok and status code not acceptable', () => {
-      let badResponse = { ok: false, status: 404 }
-      let checkNotOk = () => statusCodeCheck(badResponse)
-
-      expect(checkNotOk).to.throw(Error)
-    })
-  })
-
-  describe('given a test function for checking responses', () => {
-    let checkFoo
+    let statusCodeCheck;
 
     before(() => {
-      checkFoo = makeOkCheck({ test: res => res.foo === true })
-    })
+      statusCodeCheck = makeOkCheck({ okCodes: [403, 500] });
+    });
+
+    it('still passes response if status ok', () => {
+      const okResponse = {
+        ok: true
+      };
+
+      expect(statusCodeCheck(okResponse)).to.deep.equal(okResponse);
+    });
+
+    it('passes response if status not ok but code is acceptable', () => {
+      const fourOhThree = {
+        ok: false,
+        status: 403
+      };
+      const fiveHundred = {
+        ok: false,
+        status: 500
+      };
+
+      expect(statusCodeCheck(fourOhThree)).to.deep.equal(fourOhThree);
+      expect(statusCodeCheck(fiveHundred)).to.deep.equal(fiveHundred);
+    });
+
+    it('throws if status not ok and status code not acceptable', () => {
+      const badResponse = {
+        ok: false,i
+        status: 404
+      };
+      const checkNotOk = () => statusCodeCheck(badResponse);
+
+      expect(checkNotOk).to.throw(Error);
+    });
+  });
+
+  describe('given a test function for checking responses', () => {
+    let checkFoo;
+
+    before(() => {
+      checkFoo = makeOkCheck({ test: res => res.foo === true });
+    });
 
     it('passes response if test returns true, regardless of status', () => {
-      let fooNotOk = { ok: false, foo: true }
-      let okFoo = { ok: true, foo: true }
+      const fooNotOk = {
+        ok: false,
+        foo: true
+      };
+      const okFoo = {
+        ok: true,
+        foo: true
+      };
 
-      expect(checkFoo(fooNotOk)).to.deep.equal(fooNotOk)
-      expect(checkFoo(okFoo)).to.deep.equal(okFoo)
-    })
+      expect(checkFoo(fooNotOk)).to.deep.equal(fooNotOk);
+      expect(checkFoo(okFoo)).to.deep.equal(okFoo);
+    });
 
     it('throws if test returns false, regardless of status', () => {
-      let okNoFoo = { ok: true }
-      let notOkNoFoo = { ok: false }
-      let runCheck = res => () => checkFoo(res)
+      const okNoFoo = {
+        ok: true
+      };
+      const notOkNoFoo = {
+        ok: false
+      };
+      const runCheck = res => () => checkFoo(res);
 
-      expect(runCheck(okNoFoo)).to.throw(Error)
-      expect(runCheck(notOkNoFoo)).to.throw(Error)
-    })
-  })
-})
+      expect(runCheck(okNoFoo)).to.throw(Error);
+      expect(runCheck(notOkNoFoo)).to.throw(Error);
+    });
+  });
+});
