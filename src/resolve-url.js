357443bf9b99e9d1cb20c77446ddadbd4d4e3889
Rafael <rafael.shayvolodyan@synthasite.com>
2023-07-17 12:37:46 +0300

    Replace `url` node module usage with url-join & URLSearchParams (#17)

--- src/resolve-url.js
+++ src/resolve-url.js
@@ -1,16 +1,30 @@
 import _ from 'lodash';
-import url from 'url';
+import urlJoin from 'url-join';
 
 const resolveUrl = function(urlString, query) {
-  const parsedUrl = url.parse(urlString, true);
+  let parsedUrlQuery = {};
+  let urlWithoutParams = urlString;
+  const queryStart = urlString.lastIndexOf('?');
 
-  const mergedQuery = _.defaults({}, query, parsedUrl.query);
+  if (queryStart !== -1){
+    const paramStr = urlString.slice(queryStart);
+    parsedUrlQuery = Object.fromEntries(
+        new URLSearchParams(paramStr)
+    );
+    urlWithoutParams = urlString.slice(0, queryStart);
+  }
+
+  const mergedQuery = _.defaults({}, query, parsedUrlQuery);
   const fullQuery = _.pickBy(mergedQuery, (value) => value);
 
-  delete parsedUrl.search;
-  parsedUrl.query = fullQuery;
+  const searchParams = new URLSearchParams(fullQuery);
+  const searchParamsString = searchParams.toString();
 
-  return url.format(parsedUrl);
+  if (searchParamsString){
+    return urlJoin(urlWithoutParams, `?${searchParamsString}`);
+  }
+
+  return urlWithoutParams;
 };
 
 export default resolveUrl;
